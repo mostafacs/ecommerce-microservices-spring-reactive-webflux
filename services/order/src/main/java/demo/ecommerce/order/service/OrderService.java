4d0b5d559593c18c8d76237e6fb3cdcacdcd73ae
Mostafa Albana <engmostafa.cs@gmail.com>
2019-05-28 02:27:28 +0200

    improve coding of OrderService

--- services/order/src/main/java/demo/ecommerce/order/service/OrderService.java
+++ services/order/src/main/java/demo/ecommerce/order/service/OrderService.java
@@ -4,15 +4,11 @@ import demo.ecommerce.order.model.ShoppingCart;
 import demo.ecommerce.order.model.ShoppingCartItem;
 import demo.ecommerce.order.repository.ShoppingCartItemRepository;
 import demo.ecommerce.order.repository.ShoppingCartRepository;
-import demo.ecommerce.product.model.Product;
 import demo.ecommerce.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 
 @Service
@@ -32,21 +28,23 @@ public class OrderService {
 
         Mono<ShoppingCart> savedShoppingCart = shoppingCartRepository.save(shoppingCart);
 
+        // assign saveShoppingCart is required to fill with data enhanced from calling flatMap
         savedShoppingCart = savedShoppingCart.flatMap(savedCart -> {
 
-            shoppingCart.getCartItemList().forEach( cartItem -> {
+            shoppingCart.getCartItemList().forEach(cartItem -> {
 
-                        cartItem.setShoppingCartId(savedCart.getId());
-                        cartItem.setProductId(cartItem.getProduct().getId());
+                cartItem.setShoppingCartId(savedCart.getId());
+                cartItem.setProductId(cartItem.getProduct().getId());
             });
 
-            // calling subscribe is a must to actually save in database otherwise data will not saved to database.
-           shoppingCartItemRepository.saveAll(shoppingCart.getCartItemList()).collectList().flatMap(savedCartItem ->  {
-                savedCart.setCartItemList(savedCartItem);
-                return Mono.just(savedCartItem);
+            // calling subscribe/flatMap/... is a must to actually save in database otherwise data will not saved to database.
+            Flux<ShoppingCartItem> cartItemFlux = shoppingCartItemRepository.saveAll(shoppingCart.getCartItemList()).flatMap(this::fillCartItemWithProduct);
+
+            return cartItemFlux.collectList().flatMap(a -> {
+                savedCart.setCartItemList(a);
+                return Mono.just(savedCart);
             });
 
-            return Mono.just(savedCart);
         });
 
 
@@ -56,42 +54,39 @@ public class OrderService {
 
     public Mono<ShoppingCart> getShoppingCart(Long cartId) {
 
-      Mono<ShoppingCart> shoppingCartMono = shoppingCartRepository.findById(cartId);
-      Flux<ShoppingCartItem> shoppingCartItemFlux = shoppingCartItemRepository.getShoppingOrderCartItems(cartId);
+        Mono<ShoppingCart> shoppingCartMono = shoppingCartRepository.findById(cartId);
+        shoppingCartMono = shoppingCartMono.flatMap(this::fillCartWithCartItems);
+        return shoppingCartMono;
+    }
 
 
-      shoppingCartMono = shoppingCartMono.flatMap( icart ->  {
+    public Flux<ShoppingCart> getAllShoppingCarts() {
+        Flux<ShoppingCart> shoppingCarts = shoppingCartRepository.findAll();
+        shoppingCarts = shoppingCarts.flatMap(this::fillCartWithCartItems);
+        return shoppingCarts;
+    }
 
-          final ShoppingCart cart = icart;
 
-          cart.setCartItemList(new ArrayList<>());
-
-        Flux<ShoppingCartItem> result = shoppingCartItemFlux.flatMap(cartItem -> {
-
-              if(cartItem.getProductId() != null) {
-                  return productRepository.findById(cartItem.getProductId()).flatMap(product -> {
-                      cartItem.setProduct(product);
-                      return Mono.just(cartItem);
-                  });
-              } else  {
-                  return Mono.just(cartItem);
-              }
-          });
+    Mono<ShoppingCart> fillCartWithCartItems(ShoppingCart cart) {
+        Flux<ShoppingCartItem> shoppingCartItemFlux = shoppingCartItemRepository.getShoppingOrderCartItems(cart.getId());
+        Flux<ShoppingCartItem> result = shoppingCartItemFlux.flatMap(this::fillCartItemWithProduct);
 
         return result.collectList().flatMap(a -> {
             cart.setCartItemList(a);
             return Mono.just(cart);
         });
-
-      });
-
-
-      return shoppingCartMono;
     }
 
-    public Flux<ShoppingCart> getAllShoppingCarts() {
-        Flux<ShoppingCart> shoppingCarts = shoppingCartRepository.findAll();
-        return shoppingCarts;
+    Mono<ShoppingCartItem> fillCartItemWithProduct(ShoppingCartItem cartItem) {
+
+        if (cartItem.getProductId() != null) {
+            return productRepository.findById(cartItem.getProductId()).flatMap(product -> {
+                cartItem.setProduct(product);
+                return Mono.just(cartItem);
+            });
+        } else {
+            return Mono.just(cartItem);
+        }
     }
 
 }
